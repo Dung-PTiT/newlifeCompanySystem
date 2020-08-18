63763fd5de18dac22fdf5d1552c742a928cb0f3c
Dung-PTiT <trongdungk53@gmail.com>
2020-08-19 01:20:14 +0700

    first commit

---
+++ src/main/java/company/newlife/controller/DashboardController.java
@@ -0,0 +1,13 @@
+package company.newlife.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+
+    @GetMapping("/")
+    public String goToDashboard() {
+        return "dashboard";
+    }
+}
---
+++ src/test/java/company/newlife/NewlifeApplicationTests.java
@@ -0,0 +1,13 @@
+package company.newlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewlifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
