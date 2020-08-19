53c4532048caccb1d384e111b540c12a9db55431
Dung-PTiT <trongdungk53@gmail.com>
2020-08-19 17:16:35 +0700

    dashboard - services

--- src/main/java/company/newlife/NewlifeApplication.java
+++ src/main/java/company/newlife/NewlifeApplication.java
@@ -2,12 +2,40 @@ package company.newlife;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.CssLinkResourceTransformer;
+import org.springframework.web.servlet.resource.ResourceUrlEncodingFilter;
+import org.springframework.web.servlet.resource.VersionResourceResolver;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
-public class NewlifeApplication {
+public class NewlifeApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(NewlifeApplication.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean<ResourceUrlEncodingFilter> resourceUrlEncodingFilter() {
+        FilterRegistrationBean<ResourceUrlEncodingFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new ResourceUrlEncodingFilter());
+        registrationBean.addUrlPatterns("/*");
+        return registrationBean;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS))
+                .resourceChain(false)
+                .addResolver(new VersionResourceResolver().addContentVersionStrategy("/**"))
+                .addTransformer(new CssLinkResourceTransformer());
     }
 
 }
